da63ec19539f122ef5be3cc45a64ede61b26458b
wawerugeek <stevemuiruri84@gmail.com>
2023-07-24 01:56:19 +0300

    delete object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,7 +10,15 @@ class FileStorage:
 
     def all(self):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is not None:
+            if type(cls) == str:
+                cls = eval(cls)
+            cls_dict = {}
+            for key, value in self.__objects.items():
+                if type(value) == cls:
+                    cls_dict[key] = value
+            return cls_dict
+        return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -48,3 +56,10 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+        
+        def delete(self, obj=None):
+            """delete obj from __objects if it exits"""
+            try:
+                del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
+            except (AttributeError, KeyError):
+                pass
