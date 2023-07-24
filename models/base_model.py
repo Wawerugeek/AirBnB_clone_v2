b16104a7f2d879539d2250d2a0803fbf9010fcef
wawerugeek <stevemuiruri84@gmail.com>
2023-07-24 21:57:16 +0300

    update on base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -15,30 +15,23 @@ class BaseModel:
     
     
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            
-        else:
-            if "created_at" in kwargs:
-                kwargs['created_at'] = (datetime.strptime(kwargs['created_at'],
-                                        '%Y-%m-%dT%H:%M:%S.%f'))
-            else:
-                self.created_at = datetime.now()
-            if "updated_at" in kwargs:
-                kwargs['updated_at'] = (datetime.strptime(kwargs['updated_at'],
-                                        '%Y-%m-%dT%H:%M:%S.%f'))
-            else:
-                self.updated_at = datetime.now()
+        """Instatntiates  base model
+        **kwargs - arguments for constructor of BaseModel"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
             if "id" not in kwargs:
                 self.id = str(uuid.uuid4())
-
-            if "__class__" in kwargs:
-                del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
@@ -54,16 +47,12 @@ class BaseModel:
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        
-        if "_sa_instance_state" in dictionary:
-            del dictionary["_sa_instance_state"]
-            
+        dictionary = dict(self.__dict__)
+        dictionary["__class__"] = str(type(self).__name__)
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        if '_sa_instance_state' in dictionary.keys():
+            del dictionary['_sa_instance_state']
         return dictionary
     
     def delete(self):
