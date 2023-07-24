da39965b94f231dc05bd92af25f5a11cb78d4241
wawerugeek <stevemuiruri84@gmail.com>
2023-07-24 22:00:36 +0300

    magic file update

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
-from os import environ
+"""Creates a filestorage instance for the application"""
+from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+from os import getenv
 
-if environ.get('HBNB_TYPE_STORAGE') == "db":
-    from models.engine.db_storage import DBStorage
+
+if getenv("HBNB_TYPE_STORAGE") == "db":
     storage = DBStorage()
 else:
-    from models.engine.file_storage import FileStorage
     storage = FileStorage()
 storage.reload()
